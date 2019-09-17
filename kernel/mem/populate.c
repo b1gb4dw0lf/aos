df8b1837a98145d3783bb06e5b229135fa17f915
Antoni Stevent <itzmjauz@gmail.com>
2019-09-17 16:49:33 +0200

    allocate pages in populate.c

--- kernel/mem/populate.c
+++ kernel/mem/populate.c
@@ -11,7 +11,7 @@ struct populate_info {
 static int populate_pte(physaddr_t *entry, uintptr_t base, uintptr_t end,
     struct page_walker *walker)
 {
-	struct page_info *page = pa2page(*entry);
+	struct page_info *page = page_alloc(ALLOC_ZERO);
 	struct populate_info *info = walker->udata;
 
 	page->pp_ref += 1;//FIXME needed?
@@ -27,16 +27,15 @@ static int populate_pde(physaddr_t *entry, uintptr_t base, uintptr_t end,
 {
 	struct page_info *page = pa2page(*entry);
 	struct populate_info *info = walker->udata;
-
-	if(page->pp_order == BUDDY_4K_PAGE) {
-		ptbl_alloc(entry, base, end, walker);
-	} else if (page->pp_order == BUDDY_2M_PAGE) {
+	if(end - base >= BUDDY_2M_PAGE) {
+		page = page_alloc(ALLOC_HUGE & ALLOC_ZERO);
 		page->pp_ref += 1;
 		assert(page->pp_ref == 1);
 		physaddr_t newAddr = page2pa(page) | info->flags | PAGE_PRESENT | PAGE_HUGE;
 		*entry = newAddr;
+	} else {
+		ptbl_alloc(entry, base, end, walker);
 	}
-
 	/* LAB 3: your code here. */
 	return 0;
 }
