3332193af100744c9fb1d83b329a24a179a7c6b9
Antoni Stevent <itzmjauz@gmail.com>
2019-09-19 19:44:11 +0200

    goddddamn

--- kernel/sched/syscall.c
+++ kernel/sched/syscall.c
@@ -110,10 +110,14 @@ void syscall_handler(uint64_t syscallno, uint64_t a1, uint64_t a2, uint64_t a3,
 	frame = &cur_task->task_frame;
 
 	/* Issue the syscall. */
- 	write_msr(MSR_KERNEL_GS_BASE, (uintptr_t) this_cpu);//should maybe be this_cpu->cpu_tss->rsp[0] ?
+// 	write_msr(MSR_KERNEL_GS_BASE, (uintptr_t) this_cpu);//should maybe be this_cpu->cpu_tss->rsp[0] ?
 	frame->rax = syscall(syscallno, a1, a2, a3, a4, a5, a6);
 
 	/* Return to the current task, which should be running. */
-	task_run(cur_task);
+	#ifdef LAB3_SYSCALL
+	  sysret64(frame);
+	#else
+		task_run(cur_task);
+	#endif
 }
 
