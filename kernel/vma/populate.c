161c29d8103b11aacc2be4e8441eac9a8939907b
Kaan Kara <kaan_k193@hotmail.com>
2019-09-21 22:54:27 +0200

    implement do_populate_vma

--- kernel/vma/populate.c
+++ kernel/vma/populate.c
@@ -13,6 +13,30 @@ int do_populate_vma(struct task *task, void *base, size_t size,
 	struct vma *vma, void *udata)
 {
 	/* LAB 4: your code here. */
+
+	if (vma->vm_flags & *((int *) udata)) {
+	  return -1;
+	}
+
+	// Add pages to be able to write and read
+	populate_region(task->task_pml4, base, size,
+	    PAGE_PRESENT | PAGE_WRITE | vma->vm_flags);
+
+	// If this is not an anonymous vma
+	// TODO: Is this exclusive to executables?
+	if (vma->vm_src) {
+    // Get source file
+	  struct elf * exe_file = (struct elf *) vma->vm_src;
+
+	  // Check if it is a valid executable
+    if (exe_file->e_magic == ELF_MAGIC) {
+      memcpy(base, vma->vm_src, vma->vm_len);
+    }
+	}
+
+  // Change the protection of physical pages according to vma
+	protect_region(task->task_pml4, base, size, vma->vm_flags);
+
 	return 0;
 }
 
