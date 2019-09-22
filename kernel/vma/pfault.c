170d3486af5df7f8439c7d8f4e9bc3f4b4e568f6
Kaan Kara <kaan_k193@hotmail.com>
2019-09-22 15:34:05 +0200

    remove unnecessary prints

--- kernel/vma/pfault.c
+++ kernel/vma/pfault.c
@@ -8,19 +8,16 @@ int task_page_fault_handler(struct task *task, void *va, int flags)
 {
 	/* LAB 4: your code here. */
 
-	cprintf("Handle PID: %d PFAULT va: %p\n", task->task_pid, va);
-
+	// If it is a null pointer
 	if (!va) return -1;
 
-	cprintf("Looking for vma\n");
-
-	// If the faulting page is valid and in vma
 	struct vma * found = find_vma(NULL, NULL, &task->task_rb, va);
 
+	// If it is not in vma
 	if (!found) return -1;
 
-	int vm_flags = 0;
-
+  // If the faulting page is valid and in vma
+  int vm_flags = 0;
 	vm_flags |= flags & 2 ? PAGE_WRITE : PAGE_PRESENT;
 
 	return populate_vma_range(task, found->vm_base, found->vm_end - found->vm_base, vm_flags);
