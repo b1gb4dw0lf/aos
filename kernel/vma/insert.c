b9598b37ee812467e2b5e16182c7e960a60006b4
Kaan Kara <kaan_k193@hotmail.com>
2019-09-23 18:02:08 +0200

    implement add_vma

--- kernel/vma/insert.c
+++ kernel/vma/insert.c
@@ -98,6 +98,54 @@ struct vma *add_anonymous_vma(struct task *task, char *name, void *addr,
 	return add_executable_vma(task, name, addr, size, flags, NULL, 0);
 }
 
+static int check_consecutive_addresses(struct task * task, void * addr, size_t size) {
+  size_t num_of_pages = ROUNDUP(size, PAGE_SIZE) / PAGE_SIZE;
+  for (void * next = ROUNDDOWN(addr, PAGE_SIZE) + PAGE_SIZE;
+        next < (void *) USER_LIM && num_of_pages > 0; next += PAGE_SIZE, num_of_pages--) {
+
+    if (task_find_vma(task, next) != 0) {
+      return 0;
+    }
+
+    if (num_of_pages == 0) {
+      return 1;
+    }
+  }
+
+  return 0;
+}
+
+static void * do_vma_loop(struct task * task, size_t size, void * start, void * end) {
+  size_t num_of_pages = ROUNDUP(size, PAGE_SIZE) / PAGE_SIZE;
+  size_t found_consecutive_pages = 0;
+  void * base = NULL;
+
+  cprintf("Adding vma of size %d with %d pages\n", size, num_of_pages);
+
+  for (void * next = start;
+       end < next && next < (void *) USER_LIM ; next -= PAGE_SIZE) {
+    // If vma addr exists in task continue
+    // Also set base to null to avoid mapping non consecutive addresses
+    if (task_find_vma(task, next) != 0) {
+      found_consecutive_pages = 0;
+      base = NULL;
+      continue;
+    }
+
+    found_consecutive_pages++;
+    // Since we are searching from end to beginning
+    base = next;
+
+    if ((found_consecutive_pages == num_of_pages) && base) {
+      cprintf("Found %d pages starting from %p - %p\n",
+              found_consecutive_pages, base, base + ROUNDUP(size, PAGE_SIZE));
+      return base;
+    }
+  }
+
+  return base;
+}
+
 /* Allocates and adds a new VMA to the requested address or tries to find a
  * suitable free space that is sufficiently large to host the new VMA. If the
  * address is NULL, this function scans the address space from the end to the
@@ -110,6 +158,33 @@ struct vma *add_anonymous_vma(struct task *task, char *name, void *addr,
 struct vma *add_vma(struct task *task, char *name, void *addr, size_t size,
 	int flags)
 {
+  void * base = NULL;
+  if (!addr) {
+    // Address is null. Now, find a suitable address by scanning the address space
+
+    base = do_vma_loop(task, size, (void *) USER_LIM - PAGE_SIZE, 0);
+    return add_anonymous_vma(task, name, base, size, flags);
+
+  } else if (addr && (task_find_vma(task, addr) != 0)
+              && !check_consecutive_addresses(task, addr, size)) {
+    // VMA at that address already exists or the size does not fit
+
+    // Search from addr towards the beginning
+    base = do_vma_loop(task, size, ROUNDDOWN(addr, PAGE_SIZE), 0);
+
+    // If not found search for addr starting from end to addr
+    if (!base) {
+      base = do_vma_loop(task, ROUNDUP(size, PAGE_SIZE),
+          (void *) USER_LIM - PAGE_SIZE, ROUNDDOWN(addr, PAGE_SIZE));
+    }
+
+    return base;
+
+  }else if (addr) {
+    // VMA at that address does not exist so just insert it to the address
+    return add_anonymous_vma(task, name, addr, size, flags);
+  }
+
 	/* LAB 4: your code here. */
 	return NULL;
 }
