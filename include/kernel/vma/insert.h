b100907071409ffdd537958928cf153067339583
Kaan Kara <kaan_k193@hotmail.com>
2019-09-30 20:58:16 +0200

    add new wrapper

--- include/kernel/vma/insert.h
+++ include/kernel/vma/insert.h
@@ -10,4 +10,6 @@ struct vma *add_anonymous_vma(struct task *task, char *name, void *addr,
 	size_t size, int flags);
 struct vma *add_vma(struct task *task, char *name, void *addr, size_t size,
 	int flags);
+struct vma *add_executable_vma_v2(struct task *task, char *name, void *addr,
+                                  size_t size, int flags, void *src, size_t len, void * page_addr);
 
