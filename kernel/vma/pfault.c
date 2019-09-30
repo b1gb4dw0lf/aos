4de28840a5f7f75324cba621a2f0aa85be7c7900
Kaan Kara <kaan_k193@hotmail.com>
2019-09-30 20:59:57 +0200

    recode cow handling

--- kernel/vma/pfault.c
+++ kernel/vma/pfault.c
@@ -26,45 +26,48 @@ int task_page_fault_handler(struct task *task, void *va, int flags)
     // If it is mapped, it is probably a write on shared
     // address check flags and create a new entry?
 
-    struct page_info * mapped_page = page_lookup(task->task_pml4, va, NULL);
-
     uint64_t page_flags = 0;
-    if (found->vm_flags & VM_READ) page_flags |= PAGE_PRESENT;
-    if (found->vm_flags & VM_WRITE) page_flags |= PAGE_WRITE;
-    if (!(found->vm_flags & VM_EXEC)) page_flags |= PAGE_NO_EXEC;
+
+    if(found->vm_flags & VM_READ) page_flags |= PAGE_PRESENT;
+    if(found->vm_flags & VM_WRITE) page_flags |= PAGE_WRITE;
+    if(!(found->vm_flags & VM_EXEC)) page_flags |= PAGE_NO_EXEC;
     page_flags |= PAGE_USER;
 
-    if (mapped_page->pp_ref > 1) {
-      size_t range_size = found->vm_end - found->vm_base;
+    if (!(page_flags & vm_flags)) return -1;
+
+    struct page_info * page = page_lookup(task->task_pml4, found->vm_base, NULL);
+
+    if (page->pp_ref > 1) { // Shared page create a new one
       struct page_info * new_page;
+      if (page->pp_order == BUDDY_2M_PAGE) {
+        // 1 - Create a new page
+        new_page = page_alloc(ALLOC_HUGE | ALLOC_ZERO);
+        // 2 - Copy page context into new one
+        memcpy(page2kva(new_page), found->vm_base, HPAGE_SIZE);
+        // 3 - Change the corresponding entry
+        page_insert(task->task_pml4, new_page, found->vm_base, page_flags);
 
-      if (range_size != HPAGE_SIZE) {
-        for (size_t i = 0; i < range_size/PAGE_SIZE; ++i) {
-          new_page = page_alloc(ALLOC_ZERO);
-          memcpy(page2kva(new_page)  + (i * PAGE_SIZE),
-              found->vm_base + (i * PAGE_SIZE), PAGE_SIZE);
-          page_insert(task->task_pml4, new_page, found->vm_base  + (i * PAGE_SIZE), page_flags);
-        }
-        found->isShared = 0;
       } else {
-        panic("HPage support not available yet.\n");
+        size_t num_of_pages = (found->vm_end - found->vm_base) / PAGE_SIZE;
+
+        for (size_t i = 0; i < num_of_pages; ++i) {
+          // 1 - Create a new page
+          new_page = page_alloc(ALLOC_ZERO);
+          // 2 - Copy page context into new one
+          memcpy(page2kva(new_page), found->vm_base + (i * PAGE_SIZE), PAGE_SIZE);
+          // 3 - Change the corresponding entry
+          page_insert(task->task_pml4, new_page, found->vm_base + (i * PAGE_SIZE), page_flags);
+        }
       }
+      found->is_shared = 0;
 
-      cprintf("Printing for PID %d\n", task->task_pid);
-      dump_page_tables(task->task_pml4, PAGE_HUGE);
-
-      return 0;
-    } else { // If the page containing this region has only one ref
-      cprintf("Only one ref\n");
+    } else { // Only one ref change perms
       protect_region(task->task_pml4, found->vm_base,
-          found->vm_end - found->vm_base, page_flags);
-      found->isShared = 0;
-      cprintf("Printing for PID %d\n", task->task_pid);
-      dump_page_tables(task->task_pml4, PAGE_HUGE);
-      return 0;
+                     found->vm_end - found->vm_base, page_flags);
     }
-  } else {
-    cprintf("Populating range\n");
+
+    return 0;
+  } else { // If the range is not mapped
     return populate_vma_range(task, found->vm_base, found->vm_end - found->vm_base, vm_flags);
   }
 }
