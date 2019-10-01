edb8cc5670a9614127fedba31b19470dfe7ce431
Kaan Kara <kaan_k193@hotmail.com>
2019-10-01 20:34:50 +0200

    add more checks to wait

--- kernel/sched/wait.c
+++ kernel/sched/wait.c
@@ -32,8 +32,28 @@ pid_t sys_wait(int *rstatus)
 pid_t sys_waitpid(pid_t pid, int *rstatus, int opts)
 {
 	/* LAB 5: your code here. */
-  if (list_is_empty(&cur_task->task_children)) return -ECHILD;
+
+  // If current task does not have any children to wait on
+	if (list_is_empty(&cur_task->task_children)) return -ECHILD;
+
+	// We are not dealing with process groups and
+	// we assume this will be not called with pid -1 since
+	// it would be same as sys_wait
   if (pid <= 0) return -ECHILD;
+
+  // If specified pid does not exist
+  if (!pid2task(pid, 0)) return -ECHILD;
+
+  size_t is_child_in = 0;
+  struct list *node;
+  struct task *child;
+
+  // Check if it is an immediate childe
+  list_foreach(&cur_task->task_children, node) {
+    child = container_of(node, struct task, task_child);
+    if (child->task_pid == pid) is_child_in = 1;
+  }
+  if (!is_child_in) return -ECHILD;
 
   struct task * task = pid2task(pid, 0);
   pid_t removed_item = task->task_pid;
